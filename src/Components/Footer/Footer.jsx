65c11b7217b0c94d0903374d0043871cf889d284
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2024-05-11 16:07:00 +0600

    updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/tourguidlogo.png"
 
 const Footer = () => {
     return (
-        <footer className="px-4 pt-36 divide-y bg-gray-200 text-gray-800">
+        <footer className="px-4 py-5 divide-y bg-gray-200 text-gray-800">
             <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
                 <div className="lg:w-1/3">
                     <button className="flex justify-center space-x-3 lg:justify-start">
